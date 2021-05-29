0810e2758e69251f00f3b20160770ab0aa500cff
Matteo Gobbo <matteo.gobbo12@gmail.com>
2021-05-29 12:18:50 +0200

    create multi step component; save data;

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const [step, setStep] = useState(0);
+    const [formData, setFormData] = useState({
+        info: {},
+        user: {}
+    });
+
+    function goNextStep() {
+        setStep(previousStep => previousStep + 1);
+    }
+
+    function updateData(type, newData) {
+        setFormData((data) => {
+            return { ...data, [type]: newData };
+        });
+    }
+
+    return (
+        <div>
+            {step === 0 && <FirstStep goNextStep={goNextStep} update={updateData}></FirstStep>}
+            {step === 1 && <SecondStep goNextStep={goNextStep} update={updateData}></SecondStep>}
+            {step === 2 && <ThirdStep data={formData} update={updateData}></ThirdStep>}
+        </div>
+    );
+}
+
+function FirstStep({ update, goNextStep }) {
+    const [newData, setNewData] = useState({});
+
+    function handleClick() {
+        update("info", newData);
+        goNextStep();
+    }
+
+    return <div>
+        <label htmlFor="email">
+            Email
+            <input type="text" name="email" onChange={(e) => setNewData({...newData, email: e.target.value})}/>
+        </label>
+        <label htmlFor="password">
+            Password
+            <input type="text" name="password" onChange={(e) => setNewData({...newData, password: e.target.value})}/>
+        </label>
+        <button onClick={handleClick}>Send</button>
     </div>
-  );
+}
+
+function SecondStep({ update, goNextStep }) {
+    const [newData, setNewData] = useState({});
+
+    function handleClick() {
+        update("user", newData);
+        goNextStep();
+    }
+
+    return <div>
+        <label htmlFor="firstname">
+            First Name
+            <input type="text" name="firstname" onChange={(e) => setNewData({...newData, firstname: e.target.value})}/>
+        </label>
+        <label htmlFor="lastname">
+            Last Name
+            <input type="text" name="lastname" onChange={(e) => setNewData({...newData, lastname: e.target.value})}/>
+        </label>
+        <label htmlFor="phonenumber">
+            Phone Number
+            <input type="text" name="phonenumber" onChange={(e) => setNewData({...newData, phonenumber: e.target.value})}/>
+        </label>
+        <button onClick={handleClick}>Send</button>
+    </div>
+}
+
+function ThirdStep({ data }) {
+
+    return <div>
+        Email: {data?.info?.email}
+        First name: {data?.user?.firstname}
+        Last name: {data?.user?.lastname}
+        Phone number: {data?.user?.phonenumber}
+        <button type="submit">Submit</button>
+    </div>
 }
 
 export default App;
